83bd1379691573b7b2a2883df45a1a23b659284c
J1032 <abc@abc.com>
2023-03-29 05:17:58 -0600

    Added docker build scripts

---
+++ docker-build-release.sh
@@ -0,0 +1,2 @@
+docker compose build
+docker tag dashboard-starter-ui:develop northamerica-northeast2-docker.pkg.dev/j1032-dashboard-starter/docker/dashboard-starter-ui:develop
---
+++ docker-push.sh
@@ -0,0 +1,1 @@
+docker push northamerica-northeast2-docker.pkg.dev/j1032-dashboard-starter/docker/dashboard-starter-ui:develop
