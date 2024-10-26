3333855fdd4f838e24b560fbd37e9b31136e261b
J1032W <j1032w@users.noreply.github.com>
2024-10-25 22:02:06 -0600

    Fixed lint issue

--- src/types/chartjs-plugin-datalabels.d.ts
+++ src/types/chartjs-plugin-datalabels.d.ts
@@ -1,9 +1,7 @@
-import {ChartType} from 'chart.js';
-import {Options} from 'chartjs-plugin-datalabels/types/options';
-
+import { Options } from 'chartjs-plugin-datalabels/types/options';
 
 declare module 'chart.js' {
-  interface PluginOptionsByType<TType extends ChartType = ChartType> {
+  interface PluginOptionsByType {
     datalabels?: Options | undefined; // You can specify a more precise type if available
   }
 }
